5a0113c36fa1ad5d3cb658ad63724e6b8304264a
M.alnsour <M.alnsour@gmail.com>
2024-09-05 22:08:52 +0300

    1st site commit

--- src/nexgen.Domain/Factories/BookFactory.cs
+++ src/nexgen.Domain/Factories/BookFactory.cs
@@ -1,11 +1,5 @@
-﻿using nexgen.Domain.Entities;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using nexgen.Domain.Entities;
 
 namespace nexgen.Domain.Factories
 {
