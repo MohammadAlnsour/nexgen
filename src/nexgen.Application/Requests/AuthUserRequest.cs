db46e269f421a8fd6b4b402ddbf786f813ad900b
M.alnsour <M.alnsour@gmail.com>
2024-09-20 15:28:30 +0300

    auth user in controller

---
+++ src/nexgen.Application/Requests/AuthUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using nexgen.Application.Contracts.DTOs;
+
+namespace nexgen.Application.Requests
+{
+    public class AuthUserRequest : IRequest<AuthUserResponseDTO>
+    {
+        public string? Username { get; set; }
+        public string? PasswordHash { get; set; }
+    }
+}
--- src/nexgen.Web.RazorPages/API/AuthController.cs
+++ src/nexgen.Web.RazorPages/API/AuthController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using nexgen.Application.Contracts.ApiResponse;
+using nexgen.Application.Contracts.DTOs;
+using nexgen.Application.Requests;
+using nexgen.Domain.Consts;
+using nexgen.Shared.AppExceptions;
 
 namespace nexgen.Web.RazorPages.API
 {
@@ -7,6 +15,41 @@ namespace nexgen.Web.RazorPages.API
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly IValidator<AuthUserRequest> _validator;
+        private readonly Serilog.ILogger _logger;
+
+        public AuthController(IMediator mediator, IValidator<AuthUserRequest> validator, Serilog.ILogger logger)
+        {
+            _mediator = mediator;
+            _validator = validator;
+            _logger = logger;
+        }
+
+        [HttpPost(Name ="AuthUser")]
+        [ValidateAntiForgeryToken]
+        [EnableCors("AllowAll")]
+        public async Task<ApiResponse<AuthUserResponseDTO>> AuthUser(AuthUserRequest authUserRequest)
+        {
+            try
+            {
+                var validationResult = _validator.Validate(authUserRequest);
+                if (!validationResult.IsValid)
+                {
+                    var validationErrors = new List<ValidationError>();
+                    validationErrors = validationResult.Errors.Select(e => new ValidationError() { Code = e.ErrorCode, Message = e.ErrorMessage }).ToList();
+                    return ApiResponse<AuthUserResponseDTO>.IsFailed("validation error", "203", validationErrors);
+                }
+
+                var response = await _mediator.Send(authUserRequest);
+                return ApiResponse<AuthUserResponseDTO>.IsSuccess(response, $"user authenticated {response}", ApiStatusCodes.Success.ToString());
+            }
+            catch (Exception ex)
+            {
+                ex.HandleException(_logger);
+                return ApiResponse<AuthUserResponseDTO>.IsFailed($"error occued {ex.Message}", ApiStatusCodes.Error.ToString());
+            }
+        }
 
     }
 }
