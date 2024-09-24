e64624b932ae7cb1ab1edd444d5316cb9119c68e
M.alnsour <M.alnsour@gmail.com>
2024-09-24 16:31:26 +0300

    website changes

---
+++ src/nexgen.Web.RazorPages/Pages/Courses.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexgen.Web.RazorPages.Pages
+{
+    public class CoursesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/nexgen.Web.RazorPages/Pages/Shared/_TopMenuPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexgen.Web.RazorPages.Pages.Shared
+{
+    public class _TopMenuPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
