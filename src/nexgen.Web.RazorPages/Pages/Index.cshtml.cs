92d316ece87c1afddc3c016e3c305accc0c09a38
M.alnsour <M.alnsour@gmail.com>
2024-10-07 23:55:34 +0300

    content

--- src/nexgen.Web.RazorPages/Pages/Index.cshtml.cs
+++ src/nexgen.Web.RazorPages/Pages/Index.cshtml.cs
@@ -1,20 +1,47 @@
-using Microsoft.AspNetCore.Mvc;
+using MediatR;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using nexgen.Application.Contracts.DTOs;
+using nexgen.Application.Requests;
+using nexgen.Shared.AppExceptions;
 
 namespace nexgen.Web.RazorPages.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+       
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IMediator _mediator;
+        private readonly Serilog.ILogger _logger;
+
+        public List<CourseResponseDTO> AllCourses { get; set; }
+
+        public IndexModel(IMediator mediator, Serilog.ILogger logger)
         {
+            _mediator = mediator;
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            var courseNotFoundMsg = "Course not found!";
 
+            try
+            {
+                var getAllCourseReq = new GetAllCoursesRequest();
+                AllCourses = await _mediator.Send(getAllCourseReq);
+
+                if (AllCourses == null)
+                {
+                    ViewData["CoursesNotFound"] = courseNotFoundMsg;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.HandleException(_logger);
+                ViewData["CoursesNotFound"] = courseNotFoundMsg;
+            }
         }
+
     }
 }
