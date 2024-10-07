392fc8046f5f9d76fefd185a0a651bab6e74c3d9
M.alnsour <M.alnsour@gmail.com>
2024-10-08 00:19:31 +0300

    pages contents

---
+++ src/nexgen.Web.RazorPages/Pages/Nursing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexgen.Web.RazorPages.Pages
+{
+    public class NursingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
