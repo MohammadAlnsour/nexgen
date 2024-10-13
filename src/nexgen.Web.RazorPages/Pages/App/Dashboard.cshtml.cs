0a707d3b6e519dbef945fcac12397085d465a98c
M.alnsour <M.alnsour@gmail.com>
2024-10-13 20:33:01 +0300

    app pages

---
+++ src/nexgen.Web.RazorPages/Pages/App/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexgen.Web.RazorPages.Pages.App
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
